bb628fe6ef18762f0ea506f5ce3d5e1f45d338c3
liquiddominator <andresgoku1000@gmail.com>
2024-11-14 12:24:26 -0400

    Agregar archivos de proyecto.

---
+++ gitInformacionMia/Pages/Acerca.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gitInformacionMia.Pages
+{
+    public class AcercaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
